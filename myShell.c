f46af9e0cde2dad3e5efd628e7e20dc0e7289084
Merin Mundt <merin@iastate.edu>
2019-10-14 21:04:26 -0500

    documentation

--- myShell.c
+++ myShell.c
@@ -8,6 +8,8 @@
 #include<readline/history.h> 
 
 //This functions gets the current directory
+//There are no parameters
+//The return value is the char pointer that holds the current working directory path
  char* getDir(){
         size_t size = 256;
         char *cwd = malloc(256 * sizeof(char));
@@ -18,7 +20,7 @@
 
 //This function will get the commands from the user, and process them
 //There are no parameters
-//There are no return values
+//There return value is the variable holding the user input from the command line
 char* readInput(){
         char* input = NULL; 
         size_t size = 0;
@@ -27,11 +29,24 @@ char* readInput(){
         return input;
 }
 
-//This function will parse the user input line
-//
-//
+//This function will parse the user input line and send it to the program that will execute the instruction
+//The parameter being passed in is the line that the user has entered into the shell command linfe
+//There are no return values
 void parseLine(char* input){
+        char* token;
+        char** commands;
+        int i = 0;
 
+        //separating commands from semicolons
+        while((token = strtok(input, ";")) != NULL){
+              commands[i] = token;
+              i++;  
+        }
+        printf("%s", &input);
+        //separating arguments by spaces
+        while((token = strtok(input, " \n\t")) != NULL){
+                
+        }
 
 }
 
